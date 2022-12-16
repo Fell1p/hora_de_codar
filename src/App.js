5e1398119b9c561eaf5be518ccd0e4c19e85897c
Fell1p <luofelipe@hotmail.com>
2022-12-15 22:56:39 -0300

    Utilizando o JSX

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
 
 function App() {
+  const name = 'Felipe'
+  const newName = name.toUpperCase()
+
+  function sum(a, b){
+    return a + b
+  }
+
+  const url = 'https://via.placeholder.com/150'
+
   return (
     <div className="App">
       <h1>Olá</h1>
-      <p>Meu primeiro App</p>
+      <p>Olá, {newName}</p>
+      <p>Soma: {sum(1, 2)}</p>
+      <img src={url} alt="Minha Imagem" />
     </div>
   );
 }
