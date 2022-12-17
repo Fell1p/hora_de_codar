69dceb273e064974a4d8ebeeeb58cc45decebac0
Fell1p <luofelipe@hotmail.com>
2022-12-16 22:55:11 -0300

    O que é um componente, suas aplicações e implicações

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import HellWord from './components/HelloWord';
 
 function App() {
   const name = 'Felipe'
@@ -16,6 +17,7 @@ function App() {
       <p>Olá, {newName}</p>
       <p>Soma: {sum(1, 2)}</p>
       <img src={url} alt="Minha Imagem" />
+      <HellWord />
     </div>
   );
 }
