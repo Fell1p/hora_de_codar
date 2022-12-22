dc64abf734804e7207a25410ea7e65741bbf6676
Fell1p <luofelipe@hotmail.com>
2022-12-21 23:06:45 -0300

    Utilizando CSS

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
-import HellWord from './components/HelloWord';
 import SayMyName from './components/SayMyName';
 import Pessoa from './components/Pessoa';
+import Frase from './components/Frase';
 
 function App() {
 
@@ -10,6 +10,8 @@ function App() {
   return ( //nome utilizada a props crianda em SayMyName para utilizar sua propriedade e assim tornar o arquivo dinamico
 
     <div className="App">
+      <Frase />
+      <Frase />
       <SayMyName nome="Felipe"/> 
       <SayMyName nome={nome}/>
       <Pessoa nome="Felipe" idade="27" profissao="Analista" foto="https://via.placeholder.com/150" />
