b779b565356fb96b1366a7d1a3993b453f3c7270
Fell1p <luofelipe@hotmail.com>
2023-01-06 23:19:18 -0300

    Utilizando reindenização condicional

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 // Arquivo JSX responsavel por organizar, reaproveitar e reindenizar o componentes
 
 import './App.css';
-import Evento from './components/Eventos'
-import Form from './components/Form';
+import Condicional from './components/Condicional';
 
 function App() { //Componente responsavel pela reindenização dos componentes que compoem a aplicação
 
   return ( //nome utilizada a props crianda em SayMyName para utilizar sua propriedade e assim tornar o arquivo dinamico
 
     <div className="App">
-      <h1>Testando Eventos</h1>
-      <Evento numero = '1' /> 
-      <Form />
+      <h1>Rendenização Condicional</h1>
+      <Condicional />
     </div>
   );
 }
