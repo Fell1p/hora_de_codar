264af56b8cf61791d7e7bb75dcea3b39eec82275
Fell1p <luofelipe@hotmail.com>
2023-01-07 23:57:13 -0300

    Estilizando botão de submit e correção de css no componente Frase

--- src/components/evento/Button.js
+++ src/components/evento/Button.js
@@ -1,3 +1,5 @@
+import styles from './styles.css';
+
 export default function Button(props) {
-    return <button onClick={props.event} >{props.text}</button>
+    return <button className={styles.buttonSubmit}  onClick={props.event} >{props.text}</button>
 }
